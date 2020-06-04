a31f9fe87f62bb8614e8fc3c36894966ffcdce63
Michał Wanielista <mwanielista@o2.pl>
2020-06-04 11:41:41 +0200

    game logic works

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,24 @@ public class Main {
 		Scanner input = new Scanner(System.in);
 		TicTacToe game = new TicTacToe();
 		game.initializeBoard();
+		String player = "X";
 		do {
 			System.out.println(game.printBoard());
-			int player = input.nextInt();
+			System.out.println("Enter coordinate for rows: ");
+			int row = input.nextInt();
+			System.out.println("Enter coordinate for columns: ");
+			int column = input.nextInt();
+			
+			game.setPlay(row, column, player);
+			if(game.isGameOver()) {
+				System.out.println(game.printBoard() + "\n" + player + " wins");
+				break;
+			}
+			if(player == "X")
+				player = "0";
+			else
+				player = "X";
+			
 		}while(true);
 	}
 
--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -4,10 +4,13 @@ public class TicTacToe {
 	private String[][] board;
 	private static final int ROWS = 3;
 	private static final int COLUMNS = 3;
+	private String regex = "\\s{3}";	//3 empty spaces
 	
 	public TicTacToe(){
 		board = new String[ROWS][COLUMNS];
 	}
+	
+	
 	
 	/*
 	 * initializing game board
@@ -18,6 +21,48 @@ public class TicTacToe {
 				board[i][j] = "   ";
 			}
 		}
+	}
+	
+	/*
+	 * setting player move
+	 * @param x - for rows
+	 * @param y - for columns
+	 * @param player
+	 */
+	public void setPlay(int x, int y, String player ) {
+		if(this.board[x][y].matches(regex)) {
+			board[x][y] = " " + player + " ";
+		}
+	}
+	
+	
+	/*
+	 * Looking for end of game
+	 * @return true or false - depends on game result
+	 */
+	public boolean isGameOver() {
+		//checking rows
+		for(int i = 0; i < ROWS; i++) {
+			if(!board[i][0].matches(regex) && board[i][0].equals(board[i][1]) && board[i][1].equals(board[i][2])) {
+				return true;
+			}
+		}
+		
+		//checking columns
+		for(int j = 0; j < COLUMNS; j++) {
+			if(!board[0][j].matches(regex) && board[0][j].equals(board[1][j]) && board[1][j].equals(board[2][j])) {
+				return true;
+			}
+		}
+		
+		//checking diagonals
+		if(!board[0][0].matches(regex) && board[0][0].equals(board[1][1]) && board[1][1].equals(board[2][2]))
+			return true;
+		if(!board[0][2].matches(regex) && board[0][2].equals(board[1][1]) && board[1][1].equals(board[2][0]))
+			return true;
+		
+		//nobody won
+		return false;
 	}
 	
 	/*
